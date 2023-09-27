1ce51807ce6b8ce4620e0c91fad1059f9f3038a4
AbdulKashif17 <akkashif@ualr.edu>
2023-09-26 21:06:33 -0500

    commit

--- Household Graph/Leven.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 27 02:41:14 2022
-
-@author: onais
-"""
-import Levenshtein as lv
-lev_dist = lv.distance()
--- Household Graph/pseudopeople_dataset_generation.py
+++
@@ -1,5 +0,0 @@
-import  pseudopeople
-
-print(pseudopeople.generate_decennial_census(year=2020))
-
-print(pseudopeople.generate_decennial_census(year=2030))
